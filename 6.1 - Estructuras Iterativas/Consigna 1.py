0a0c82a00f7f5fc72c8e3458e667746af5b6e99f
Dalmiro <dalmirovilca@gmail.com>
2024-05-26 19:40:37 -0300

    Delete 6.1 - Estructuras Iterativas directory (reorganizacion)

--- 6.1 - Estructuras Iterativas/Consigna 1.py
+++
@@ -1,7 +0,0 @@
-# Mostrar los números desde el 0 al número solicitado al usuario (input)
-
-numerosolicitado = int(input('Ingrese un numero mayor a 0: '))
-
-for i in range (numerosolicitado):
-    i = (i+1)
-    print(i)
--- 6.1 - Estructuras Iterativas/Consigna 3.py
+++
@@ -1,7 +0,0 @@
-# Pedir que el usuario ingrese (input) nombre de personas y mostrarlos hasta que el valor
-# de lo que ingresa sea “fin”
-
-nombres = str
-
-while(nombres != 'fin'):
-    nombres = input('Ingrese nombres de personas: ')
